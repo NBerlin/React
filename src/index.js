37c02b30d276aa36af9439f227acc68c56e1cffb
NBerlin <nickiberlinpolhem@gmail.com>
2019-11-09 16:00:36 +0100

    -

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Square(props) {
+    return (
+      <button 
+      className = "square" 
+      onClick = { props.onClick }> 
+        {props.text}
+      </button>
+    );
+}
+
+class Board extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      squaresFrontside: ["test","Nikzquita"],
+      squaresBackside: ["hue", "nue"],
+      squaresFacingFront:[true, true]
+    }
+  }
+
+  onClick(i){
+    const squaresFacingFrontCopy = this.state.squaresFacingFront;
+    squaresFacingFrontCopy[i] = !squaresFacingFrontCopy[i];
+    this.setState({
+      squaresFacingFront: squaresFacingFrontCopy,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+    <Square 
+        text={this.state.squaresFacingFront[i] ? this.state.squaresFrontside[i] : this.state.squaresBackside[i]}
+        onClick={() => this.onClick(i)}
+    />
+    );
+  }  
+
+
+  render() {
+  const items = []
+
+  for (const[index, value] of this.state.squaresFrontside.entries()){
+    items.push(this.renderSquare(index))
+  }
+    return (
+  <div>
+    {items}
+  </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Board />,
+  document.getElementById('root')
+);
