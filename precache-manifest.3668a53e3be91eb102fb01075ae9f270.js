00e11932fed6dce81acdc45315227a5d83e6f037
NBerlin <nickiberlinpolhem@gmail.com>
2019-11-14 14:30:42 +0100

    Updates

---
+++ precache-manifest.3668a53e3be91eb102fb01075ae9f270.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30c015c93f59a1fdfbfbbab93904821",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "183a084b28f8d89641d0",
+    "url": "/React/static/css/main.e578bbe2.chunk.css"
+  },
+  {
+    "revision": "88212f146e8ac0d78e30",
+    "url": "/React/static/js/2.c1d901e8.chunk.js"
+  },
+  {
+    "revision": "183a084b28f8d89641d0",
+    "url": "/React/static/js/main.90d4a313.chunk.js"
+  },
+  {
+    "revision": "59cbeb86dcbaa02784b8",
+    "url": "/React/static/js/runtime-main.87126200.js"
+  }
+]);
